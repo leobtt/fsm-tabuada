a1300794fb30c619d2767b2e63f6f97fb30c9bc8
leobtt <leonarbat8@gmail.com>
2022-04-21 16:09:04 -0300

    initial Commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { application } from 'express'
+import path from 'path'
+import { router } from './routes'
+const port = 3333
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(router)
+
+app.listen(port, () => { 
+  console.log(`tabuada is listennig the port ${port}`)
+})
